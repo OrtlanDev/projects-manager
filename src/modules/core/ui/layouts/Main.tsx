4c9ad5175839c6feb7f6bacfeaa890c853a475a8
Orlando <ortlandev@gmail.com>
2025-04-27 20:57:37 -0400

    bugFixedz

--- src/modules/core/ui/layouts/Main.tsx
+++ src/modules/core/ui/layouts/Main.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/modules/core/ui/components/shadcn/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/modules/core/ui/components/shadcn/dialog";
 import { SidebarProvider, SidebarTrigger } from "@/modules/core/ui/components/shadcn/sidebar";
-import AppSidebar from "@/modules/navigation/ui/components/appSidebar";
+import AppSidebar from "@/modules/navigation/ui/components/AppSidebar";
 import ProjectForm from "@/modules/projects/ui/components/ProjectForm";
 import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { Outlet } from "react-router-dom";
